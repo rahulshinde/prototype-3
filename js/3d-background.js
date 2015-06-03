9fa58f32f50d968bd592785d41a227bb1ee05e43
rahul shinde <rshinde@mica.edu>
2015-06-03 19:47:26 -0400

    testing light

--- js/3d-background.js
+++ js/3d-background.js
@@ -1,26 +1,80 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 100, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var scene,
+	camera,
+	light1,
+	renderer,
+	cube;
 
-var renderer = new THREE.WebGLRenderer({ alpha: true });
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+var mouseX = 0, mouseY = 0;
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.LineBasicMaterial( { color:0x404040 } );
-var cube = new THREE.Mesh( geometry, material );
-var light = new THREE.AmbientLight( 0x404040 );
-scene.add( light );
-scene.add( cube );
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
 
-camera.position.z = 5;
+init();
+animate();
 
-var render = function () {
-	requestAnimationFrame( render );
+function init() {
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 1000 );
+	camera.position.z = 100;
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.1;
+	var sphere = new THREE.SphereGeometry( 0.4, 16, 8 );
+
+	light1 = new THREE.PointLight( 0x2defff, 2, 9000 );
+	light1.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0x2defff } ) ) );
+	light1.position.set( 0, 0, 50 );
+	scene.add( light1 );
+
+	scene.add( new THREE.AmbientLight( 0x000000 ) );
+
+	renderer = new THREE.WebGLRenderer({ alpha: true });
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
+
+	var geometry = new THREE.BoxGeometry( 50, 30, 40 );
+	var material = new THREE.MeshPhongMaterial({color: 0x696969, emissive: 0x696969, specular:0x696969, shininess: 15, side: THREE.DoubleSide});
+	cube = new THREE.Mesh( geometry, material );
+	scene.add( cube );
+
+	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
+	window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+function onWindowResize() {
+
+	windowHalfX = window.innerWidth / 2;
+	windowHalfY = window.innerHeight / 2;
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function onDocumentMouseMove( event ) {
+
+	mouseX = ( event.clientX - windowHalfX );
+	mouseY = ( event.clientY - windowHalfY );
+
+}
+
+function animate() {
+
+	requestAnimationFrame( animate );
+
+	render();
+
+}	
+
+function render() {
+	light1.position.x += mouseX / 900;
+	light1.position.y += - mouseY / 900;
+
+	console.log(light1.position.x);
+
+	camera.lookAt( scene.position );
 
 	renderer.render(scene, camera);
 };
-
-render();
