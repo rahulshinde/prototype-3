008bd2060c05b6cb09063c63c905a122dc5a014d
rahul shinde <rshinde@mica.edu>
2015-08-21 22:16:31 -0600

    changing color

--- js/2/3d-background.js
+++ js/2/3d-background.js
@@ -17,8 +17,6 @@ var mouseX = 0, mouseY = 0;
 var pts = [];
 var closedSpline;
 
-var x = 20;
-
 var container = document.getElementById( 'three-container' );
 
 init();
@@ -28,18 +26,10 @@ function init() {
 	scene = new THREE.Scene();
 	camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 1000 );
 	camera.position.z = 100;
-	var cameraControls;
 
 	renderer = new THREE.WebGLRenderer({ alpha: true });
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	container.appendChild( renderer.domElement );
-
-	cameraControls = new THREE.OrbitControls(camera, renderer.domElement);
-	cameraControls.target.set( 0, 0, 0);
-	cameraControls.maxDistance = 400;
-	cameraControls.minDistance = 30;
-	cameraControls.update();
-
 
 	//adding lights, sphere is just to check light position.
 	var sphere = new THREE.SphereGeometry( 0.4, 16, 8 );
@@ -51,7 +41,7 @@ function init() {
 
 	light2 = new THREE.PointLight( 0xf8ffa8, 1, 4500 );
 	light2.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xf8ffa8 } ) ) );
-	light2.position.set( 10, 0, -50 );
+	light2.position.set( 10, 30, 10 );
 	scene.add( light2 );
 
 	scene.add( new THREE.AmbientLight( 0x000000 ) );
@@ -70,7 +60,7 @@ function init() {
 		// 3d shape
 
 		var geometry = new THREE.ExtrudeGeometry( shape, extrudeSettings );
-		var material = new THREE.MeshPhongMaterial({ map:THREE.ImageUtils.loadTexture('../../textures/arch.jpg') , shininess: 15, side: THREE.DoubleSide});
+		var material = new THREE.MeshPhongMaterial({transparent:true, opacity: 0.6, color: 0xc38e3c, emissive: 0xc38e3c, specular:0x696969, shininess: 5, side: THREE.DoubleSide});
 
 		var mesh = new THREE.Mesh( geometry, material );
 		mesh.position.set( x, y, z);
@@ -86,13 +76,15 @@ function init() {
 	pts.push( new THREE.Vector2 ( -20, 30 ) );
 	pts.push( new THREE.Vector2 ( 0, -30 ) );
 	pts.push( new THREE.Vector2 ( 10, -30 ) );
+	pts.push( new THREE.Vector2 ( 30, 0 ) );
 
 	var shape = new THREE.Shape( pts );
 
 	closedSpline = new THREE.ClosedSplineCurve3( [
 		new THREE.Vector3( 0, 0, 0 ),
 		new THREE.Vector3( 10, 10, -10 ),
-		new THREE.Vector3( 20, -10, -20 )
+		new THREE.Vector3( 20, -10, -20 ),
+		new THREE.Vector3( 0, 0, 0 ),
 	] );
 
 	var extrudeSettings = { amount: 100, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1, extrudePath: closedSpline };
@@ -118,14 +110,7 @@ function onDocumentMouseMove( event ) {
 
 	mouseX = ( event.clientX - windowHalfX );
 	mouseY = ( event.clientY - windowHalfY );
-	modularPoints();
 
-}
-
-function modularPoints () {
-	console.log(closedSpline.points[0]);
-	pts[0].set( x +=1 , 30);
-	x + 1;
 }
 
 function animate() {
@@ -137,6 +122,9 @@ function animate() {
 
 function render() {
 	light1.position.set ( (mouseX - light1.position.x) * 0.075, - (mouseY - light1.position.y) * 0.075, 50);
+
+	group.rotation.x += .002;
+	group.rotation.y += .001;
 
 	renderer.render(scene, camera);
 };
