9b88e1fd39100bac39629865b1e482b72646d55f
rahul shinde <rshinde@mica.edu>
2015-08-24 17:19:36 -0600

    video textures not working too well

--- js/2/environment.js
+++ js/2/environment.js
@@ -186,43 +186,6 @@ function init() {
 
 	raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
 
-	///////////
-	// VIDEO //
-	///////////
-	
-	// create the video element
-	video = document.createElement( 'video' );
-	// video.id = 'video';
-	// video.type = ' video/ogg; codecs="theora, vorbis" ';
-	video.src = "../../video/snake-flag.mp4";
-	video.load(); // must call after setting/changing source
-	video.play();
-	
-	// alternative method -- 
-	// create DIV in HTML:
-	// <video id="myVideo" autoplay style="display:none">
-	//		<source src="videos/sintel.ogv" type='video/ogg; codecs="theora, vorbis"'>
-	// </video>
-	// and set JS variable:
-	// video = document.getElementById( 'myVideo' );
-	
-	videoImage = document.createElement( 'canvas' );
-	videoImage.width = 480;
-	videoImage.height = 204;
-
-	videoImageContext = videoImage.getContext( '2d' );
-	// background color if no video present
-	videoImageContext.fillStyle = '#000000';
-	videoImageContext.fillRect( 0, 0, videoImage.width, videoImage.height );
-
-	videoTexture = new THREE.Texture( videoImage );
-	videoTexture.minFilter = THREE.LinearFilter;
-	videoTexture.magFilter = THREE.LinearFilter;
-	
-	var movieMaterial = new THREE.MeshBasicMaterial( { map: videoTexture, overdraw: true, side:THREE.DoubleSide } );
-	// the geometry on which the movie will be displayed;
-	// 		movie image will be scaled to fit these dimensions.
-
 	////////////////////////
 	//////////scene/////////
 	////////////////////////
@@ -232,7 +195,7 @@ function init() {
 	// S C R E E N
 
 	var geometry1 = new THREE.BoxGeometry( 10, 7, 0.1 );
-	var cube1 = new THREE.Mesh( geometry1, movieMaterial );
+	var cube1 = new THREE.Mesh( geometry1, material );
 	scene.add( cube1 );
 	cube1.position.y = 13;
 	cube1.position.z = -10;
